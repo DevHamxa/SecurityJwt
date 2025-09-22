5841fadf31493df581877f308ba41518417302bf
Adil Waheed <adil@cognitionads.com>
2025-09-23 04:12:30 +0500

    11th commit

--- src/main/java/org/example/securitydemo/StrictCsrfHeaderFilter.java
+++ src/main/java/org/example/securitydemo/StrictCsrfHeaderFilter.java
@@ -4,16 +4,19 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 
 public class StrictCsrfHeaderFilter extends OncePerRequestFilter {
+    private final CsrfTokenRepository tokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
     private static final List<String> CSRF_IGNORED = List.of(
             "/user/login",
-            "/user/signup",
-            "/autocsr/csrf-token"
+            "/user/signup"
     );
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -28,11 +31,11 @@ public class StrictCsrfHeaderFilter extends OncePerRequestFilter {
                 "PUT".equalsIgnoreCase(request.getMethod()) ||
                 "DELETE".equalsIgnoreCase(request.getMethod())) {
 
-            String header = request.getHeader("X-XSRF-TOKEN");
-            if (header == null || header.isBlank()) {
+            CsrfToken csrfToken = tokenRepository.loadToken(request);
+            if (csrfToken == null || !csrfToken.getToken().equals(request.getHeader("X-XSRF-TOKEN"))) {
                 response.setStatus(HttpServletResponse.SC_FORBIDDEN);
                 response.setContentType("application/json");
-                response.getWriter().write("{\"error\":\"Missing CSRF header\"}");
+                response.getWriter().write("{\"error\":\"Forbidden - Invalid CSRF token\"}");
                 return;
             }
         }
